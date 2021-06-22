46dd9a41ef672d11323d4df31fe1a9e57875915f
Kenton Roberts <kentandrewr@gmail.com>
2021-06-22 10:15:09 -0500

    Made further adjustments to code, added default variables, and added docstrings to some of the functions.

--- PetriFI.ino
+++ PetriFI.ino
@@ -1,18 +1,17 @@
 /*
- * Team Petri-FI | SEED Internship & Rice360 Institute
- *
- * Contributors:
- * Kenton Roberts
- * Nancy Lindsey
- * Teja Paturu
- * Sara Barker
- * 
- * Code Adapted From Previous Student Teams:
- * Moonrat
- * Minicubator
- * and Contributions from the Public Invention Moonrat Team.
- *
- */
+  Team Petri-FI | SEED Internship & Rice360 Institute
+  
+  Contributors:
+  Kenton Roberts
+  Nancy Lindsey
+  Teja Paturu
+  Sara Barker
+  
+  Code Adapted From Previous Student Teams:
+  Moonrat
+  Minicubator
+  and Contributions from the Public Invention Moonrat Team.
+*/
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1351.h>
@@ -20,7 +19,7 @@
 
 // Screen dimensions
 #define SCREEN_WIDTH  128
-#define SCREEN_HEIGHT 128 // Change this to 96 for 1.27" OLED.
+#define SCREEN_HEIGHT 128 // FIXME Change this to 96 for 1.27" OLED.
 
 // You can use any (4 or) 5 pins
 #define SCLK_PIN 2
@@ -29,11 +28,11 @@
 #define CS_PIN   5
 #define RST_PIN  6
 
-//define buttons
+// define buttons
 #define Up 11
 #define Down 9
 
-//define transistor/power from battery to heating pad
+// define transistor/power from battery to heating pad
 #define transistor 12
 #define led 13
 #define piezo_alarm 15
@@ -49,8 +48,14 @@
 #define YELLOW          0xFFE0
 #define WHITE           0xFFFF
 
+// Incubation Defaults
+#define DEFAULT_TEMP 30 // degrees Celsius
+#define MAX_TEMP_SET 42 // degrees Celsius; max temperature that the user can set during incubation
+#define MIN_TEMP_SET 30 // degrees Celsius; minimum temperature that the user can set during incubation
+#define DEFAULT_TIME 48 // hours
 
-Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, CS_PIN, DC_PIN, MOSI_PIN, SCLK_PIN, RST_PIN);
+// Defines the parameters associated with the TFT screen.
+Adafruit_SSD1351 tft_screen = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, CS_PIN, DC_PIN, MOSI_PIN, SCLK_PIN, RST_PIN);
 
 bool firstTempSet = true;
 bool firstTimeSet = true;
@@ -60,21 +65,21 @@ int ThermistorPin = 0;
 int Vo;
 float R1 = 10000;
 float logR2, R2, T;
-float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07; // What are these?
 int finaltest = 0;
 int menuButtonPreviousState = LOW;
 int tempUpPreviousState = LOW;
 int tempDownPreviousState = LOW;
 int out_range_counter = 0;
 int menuOpt = 0;
-float runningAvg[] = {0, 0, 0, 0, 0}; // keeps a running average, maybe for displaying the temp values? -kenton
+float runningAvg[] = {0, 0, 0, 0, 0};
 float T_average;
 //int range = map(T, tempMin, tempMax, 0, 3);
 
-int T_setting = 30; // default temperature setting is 30C, use button to increment or decrement as necessary
-int T_setting_prev = 30; // default temperature setting is 30C, use button to increment or decrement as necessary
-int time_setting = 48; // default time setting is 48 hours, use button to increment or decrement as necessary
-int time_setting_prev = 48; // default time setting is 48 hours, use button to increment or decrement as necessary
+int T_setting = DEFAULT_TEMP; // use button to increment or decrement as necessary
+int T_setting_prev = DEFAULT_TEMP;
+int time_setting = DEFAULT_TIME; // default time setting is 48 hours, use button to increment or decrement as necessary
+int time_setting_prev = DEFAULT_TIME; // default time setting is 48 hours, use button to increment or decrement as necessary
 
 int DownState = 0;
 int UpState = 0;
@@ -93,13 +98,13 @@ void setup(void) {
   
   Serial.begin(9600);
   Serial.println("hello!"); //FIXME improve intro text -kenton
-  tft.begin();
-  tft.fillScreen(BLACK);
+  tft_screen.begin();
+  tft_screen.fillScreen(BLACK);
 
   Serial.println("init"); //FIXME improve this, too -kenton
 
   uint16_t time = millis();
-  tft.fillRect(0, 0, 128, 128, BLACK);
+  tft_screen.fillRect(0, 0, 128, 128, BLACK);
   time = millis() - time;
 }
 
@@ -113,8 +118,8 @@ void loop() {
   if (selectButtonState == HIGH) { // pressing Down will increment menuopt by 1 
 	 // TODO we should make a comment table explaining what menu options mean what -kenton.
     menuOpt = (menuOpt + 1) % 3; // increment the menu setting by 1
-    //tft.fillScreen(BLACK);
-    tft.fillRect(0, 40, 128, 60, BLACK);
+    //tft_screen.fillScreen(BLACK);
+    tft_screen.fillRect(0, 40, 128, 60, BLACK);
   }
   if (menuOpt == 0) {
     setTemp(menuOpt);
@@ -122,7 +127,7 @@ void loop() {
   else if(menuOpt == 1) {
     setTime(menuOpt);
   }
-  else { // only entered if menuOpt is 2. -kenton
+  else { // only entered if menuOpt is 2
     runningAvg[0] = T_setting;
     runningAvg[1] = T_setting;
     runningAvg[2] = T_setting;
@@ -135,56 +140,65 @@ void loop() {
 
 void printBanner(int T_setting, int T_setting_prev, int time_setting, int time_setting_prev) {
 //prints the yellow text at the top that shows the selected setting while powered on.
-  tft.setCursor(0,10);
-  tft.setTextColor(YELLOW);
-  tft.print("Curr Temp Set (C): "); // Temperature set prompt
-  tft.print(T_setting);
+  tft_screen.setCursor(0,10);
+  tft_screen.setTextColor(YELLOW);
+  tft_screen.print("Curr Temp Set (C): "); // Temperature set prompt
+  tft_screen.print(T_setting);
   
-  tft.setCursor(0,20);
-  tft.setTextColor(YELLOW);
-  tft.print("Curr Time Set (h): "); // Time set prompt
-  tft.print(time_setting);
+  tft_screen.setCursor(0,20);
+  tft_screen.setTextColor(YELLOW);
+  tft_screen.print("Curr Time Set (h): "); // Time set prompt
+  tft_screen.print(time_setting);
   
 }
 
 void setTemp(int m) { // menu option to set temperature
+  /*
+    Sets the temperature for the next incubation period.
+  */
+
   // where the heck is "int m" used in this method? -kenton
   DownState = digitalRead(Down);
   UpState = digitalRead(Up);
 
   int T_setting_prev = T_setting;
   
-  if ((DownState == HIGH) && (T_setting <= 42) && (T_setting > 30)) { 
+  if ((DownState == HIGH) && (T_setting <= MAX_TEMP_SET) && (T_setting > MIN_TEMP_SET)) { 
     T_setting--; // decrement the temperature setting by 1, given within the acceptable range
 
   }
-  if ((UpState == HIGH) && (T_setting < 42) && (T_setting >= 30)) {
+  if ((UpState == HIGH) && (T_setting < MAX_TEMP_SET) && (T_setting >= MIN_TEMP_SET)) {
     T_setting++; // increment the temperature setting by 1, given within the acceptable range
     
   }  
 
   if ((T_setting != T_setting_prev) || (firstTempSet == true)) {
-    tft.setCursor(0,10); // this code blots out the yellow print at the top while the temperature selection is occurring
-    tft.setTextColor(YELLOW);
-    tft.print("Curr Temp Set (C): ");
-    tft.setTextColor(BLACK);
-    tft.print(T_setting_prev);
-    tft.setCursor(0,50); // display the selection in progress to the user.
-    tft.setTextColor(WHITE);
-    tft.println("Set temperature (C): ");
-    tft.setCursor(0,60);
-    tft.setTextColor(BLACK);
-    tft.print(T_setting_prev);
-    tft.setCursor(0,60);
-    tft.setTextColor(MAGENTA);
-    tft.print(T_setting);
+    tft_screen.setCursor(0,10); // this code blots out the yellow print at the top while the temperature selection is occurring
+    tft_screen.setTextColor(YELLOW);
+    tft_screen.print("Curr Temp Set (C): ");
+    tft_screen.setTextColor(BLACK);
+    tft_screen.print(T_setting_prev);
+    tft_screen.setCursor(0,50); // display the selection in progress to the user.
+    tft_screen.setTextColor(WHITE);
+    tft_screen.println("Set temperature (C): ");
+    tft_screen.setCursor(0,60);
+    tft_screen.setTextColor(BLACK);
+    tft_screen.print(T_setting_prev);
+    tft_screen.setCursor(0,60);
+    tft_screen.setTextColor(MAGENTA);
+    tft_screen.print(T_setting);
   }
 
   //delay(250);
   firstTempSet = false;
 }
 
-void setTime(int m) { // menu option to set time
+void setTime(int m) {
+  /*
+    Activates the menu option to set the duration of incubation.
+	 Modifies 
+
+  */
   
   DownState = digitalRead(Down);
   UpState = digitalRead(Up);
@@ -199,20 +213,20 @@ void setTime(int m) { // menu option to set time
   }  
 
   if ((time_setting != time_setting_prev) || (firstTimeSet == true)) {
-    tft.setCursor(0,20); // this code blots out the yellow print at the top while the time selection is occurring
-    tft.setTextColor(YELLOW);
-    tft.print("Curr Time Set (h): ");
-    tft.setTextColor(BLACK);
-    tft.print(time_setting_prev);
-    tft.setCursor(0,50); // display the selection in progress to the user.
-    tft.setTextColor(WHITE);
-    tft.println("Set time (hrs): ");
-    tft.setCursor(0,60);
-    tft.setTextColor(BLACK);
-    tft.print(time_setting_prev);
-    tft.setCursor(0,60);
-    tft.setTextColor(MAGENTA);
-    tft.print(time_setting);
+    tft_screen.setCursor(0,20); // this code blots out the yellow print at the top while the time selection is occurring
+    tft_screen.setTextColor(YELLOW);
+    tft_screen.print("Curr Time Set (h): ");
+    tft_screen.setTextColor(BLACK);
+    tft_screen.print(time_setting_prev);
+    tft_screen.setCursor(0,50); // display the selection in progress to the user.
+    tft_screen.setTextColor(WHITE);
+    tft_screen.println("Set time (hrs): ");
+    tft_screen.setCursor(0,60);
+    tft_screen.setTextColor(BLACK);
+    tft_screen.print(time_setting_prev);
+    tft_screen.setCursor(0,60);
+    tft_screen.setTextColor(MAGENTA);
+    tft_screen.print(time_setting);
   }
 
   firstTimeSet = false;
@@ -220,10 +234,17 @@ void setTime(int m) { // menu option to set time
 
 
 float readTemp() {
-  Vo = analogRead(ThermistorPin);
+  /*
+    Performs mathematical operations on the voltage reading from the thermistor pin.
+    Calculates Kelvin temperature, then outputs as a Celsius reading.
+    
+    Returns: T - Celsius temperature reading from temperature sensor
+  */
+
+  Vo = analogRead(ThermistorPin); // reads the voltage of ThermistorPin. Resolution of only 4.9 mV (out of 5V)
   R2 = R1 * (1023.0 / (float)Vo - 1.0);
   logR2 = log(R2);
-  T = (1.0 / (c1 + c2 * logR2 + c3 * logR2 * logR2 * logR2));
+  T = 1.0 / (c1 + (c2 * logR2) + (c3 * pow(logR2, 3)));
   T = T - 273.15;
   return T;
 }
@@ -281,40 +302,40 @@ void startRun() { // ask user whether to start run (we are not doing this curren
     delay(500);
     //resets screen words to black (clears characters)
     printBanner(T_setting, T_setting_prev, time_setting, time_setting_prev);
-    tft.setCursor(0, 43);
-    tft.setTextColor(BLACK);
-    tft.print(T);
-    tft.setCursor(30, 51);
-    tft.setTextColor(BLACK); //blacks out the screen from the previous temperature measurement
-    tft.print("IN RANGE");
-    tft.setCursor(30, 51);
-    tft.setTextColor(BLACK);
-    tft.print("OUT OF RANGE");
-    tft.setCursor(0, 59);
-    tft.setTextColor(WHITE);
-    tft.println("Finished Test was:"); // information on whether the internal temperature is at the correct temp over a trend of time
+    tft_screen.setCursor(0, 43);
+    tft_screen.setTextColor(BLACK);
+    tft_screen.print(T);
+    tft_screen.setCursor(30, 51);
+    tft_screen.setTextColor(BLACK); //blacks out the screen from the previous temperature measurement
+    tft_screen.print("IN RANGE");
+    tft_screen.setCursor(30, 51);
+    tft_screen.setTextColor(BLACK);
+    tft_screen.print("OUT OF RANGE");
+    tft_screen.setCursor(0, 59);
+    tft_screen.setTextColor(WHITE);
+    tft_screen.println("Finished Test was:"); // information on whether the internal temperature is at the correct temp over a trend of time
     if (finaltest == 1) { // build in the audio and visual alarms data here
-      tft.setCursor(0, 67); 
-      tft.setTextColor(BLACK);
-      tft.print("SUCCESSFUL"); // what does "successful" and "not successful" mean here? -kenton
-      tft.setCursor(0, 67);
-      tft.setTextColor(WHITE);
-      tft.print("NOT SUCCESSFUL");
+      tft_screen.setCursor(0, 67); 
+      tft_screen.setTextColor(BLACK);
+      tft_screen.print("SUCCESSFUL"); // what does "successful" and "not successful" mean here? -kenton
+      tft_screen.setCursor(0, 67);
+      tft_screen.setTextColor(WHITE);
+      tft_screen.print("NOT SUCCESSFUL");
 
       // trigger alarm by activating the LED and piezo
       digitalWrite(led, HIGH);
       //tone(piezo_alarm, 1000, 1000);
     }
     else {
-      tft.setCursor(0, 67);
-      tft.setTextColor(WHITE);
-      tft.print("SUCCESSFUL");
+      tft_screen.setCursor(0, 67);
+      tft_screen.setTextColor(WHITE);
+      tft_screen.print("SUCCESSFUL");
     }
 
     //reads current temperature
     T = readTemp();
 
-    // tft print function
+    // tft_screen print function
     tftPrintTest();
 
     transistorControl();
@@ -333,55 +354,64 @@ void transistorControl(){
     //turn off transistor, stop power flow from battery to the heating pad
     digitalWrite(transistor, LOW);
     if (onStatus == 1) {
-      tft.fillRect(67, 80, 128, 87, BLACK);
+      tft_screen.fillRect(67, 80, 128, 87, BLACK);
     }
-    tft.setCursor(0,80);
-    tft.setTextColor(WHITE);
-    tft.setTextSize(0);
-    tft.println("HEATING PAD OFF");
+    tft_screen.setCursor(0,80);
+    tft_screen.setTextColor(WHITE);
+    tft_screen.setTextSize(0);
+    tft_screen.println("HEATING PAD OFF");
     onStatus = 0;
   } else if (T < T_setting-0.5) {
     //turn on transistor, start power flow from battery to the heating pad if in range or below range
     digitalWrite(transistor, HIGH); // Cory's note: switched from high to low here
     if (onStatus == 0) {
-      tft.fillRect(67, 80, 128, 87, BLACK);
+      tft_screen.fillRect(67, 80, 128, 87, BLACK);
     }
-    tft.setCursor(0,80);
-    tft.setTextColor(WHITE);
-    tft.setTextSize(0);
-    tft.println("HEATING PAD ON");
+    tft_screen.setCursor(0,80);
+    tft_screen.setTextColor(WHITE);
+    tft_screen.setTextSize(0);
+    tft_screen.println("HEATING PAD ON");
     onStatus = 1;
   }
 }
 
-// used in the startUp() method to initialize the screen, it appears -kenton
 void tftPrintStartUp() {
-  tft.setCursor(0, 35);
-  tft.setTextColor(WHITE);
-  tft.setTextSize(0);
-  tft.println("Current Temperature:");
-  tft.setTextColor(RED);
-  tft.println(T);
-  tft.setCursor(0, 51);
-  tft.setTextColor(WHITE);
-  tft.setCursor(0, 51);
-  tft.print("PRE-HEATING");
-  tft.setCursor(0, 61);
-  tft.print("Please wait");
+  /*
+    This method starts the tft_screen and prints the startup text.
+
+	 Returns: none
+  */
+
+  tft_screen.setCursor(0, 35);
+  tft_screen.setTextColor(WHITE);
+  tft_screen.setTextSize(0);
+  tft_screen.println("Current Temperature:");
+  tft_screen.setTextColor(RED);
+  tft_screen.println(T);
+  tft_screen.setCursor(0, 51);
+  tft_screen.setTextColor(WHITE);
+  tft_screen.setCursor(0, 51);
+  tft_screen.print("PRE-HEATING");
+  tft_screen.setCursor(0, 61);
+  tft_screen.print("Please wait");
 }
 
-// unclear how this is different from tftPrintStart(), but I'm assuming it erases something -kenton
 void tftEraseStartUp() {
-  tft.setCursor(0, 35);
-  tft.setTextColor(BLACK);
-  tft.setTextSize(0);
-  tft.println("Current Temperature:");
-  tft.println(T);
-  tft.setCursor(0, 51);
-  tft.setCursor(0, 51);
-  tft.print("PRE-HEATING");
-  tft.setCursor(0, 61);
-  tft.print("Please wait");
+  /*
+    Erases the start-up text by overwriting it with BLACK text.
+
+	 Returns: none
+  */
+  tft_screen.setCursor(0, 35);
+  tft_screen.setTextColor(BLACK);
+  tft_screen.setTextSize(0);
+  tft_screen.println("Current Temperature:");
+  tft_screen.println(T);
+  tft_screen.setCursor(0, 51);
+  tft_screen.setCursor(0, 51);
+  tft_screen.print("PRE-HEATING");
+  tft_screen.setCursor(0, 61);
+  tft_screen.print("Please wait");
 }
 
 void tftPrintTest() {
@@ -399,26 +429,26 @@ void tftPrintTest() {
   }
   T_average = sum / 5;
   
-  tft.setCursor(0, 35);
-  tft.setTextColor(WHITE);
-  tft.setTextSize(0);
-  tft.println("Current Temperature:");
-  tft.setTextColor(RED);
-  tft.println(T);
-  tft.setCursor(0, 51);
+  tft_screen.setCursor(0, 35);
+  tft_screen.setTextColor(WHITE);
+  tft_screen.setTextSize(0);
+  tft_screen.println("Current Temperature:");
+  tft_screen.setTextColor(RED);
+  tft_screen.println(T);
+  tft_screen.setCursor(0, 51);
   //reads stored temperature and determines if the temperature falls within range
-  tft.setTextColor(WHITE);
-  tft.print("Test:");
+  tft_screen.setTextColor(WHITE);
+  tft_screen.print("Test:");
   if (T_average > T_setting-2 && T_average < T_setting+2) { // this is if the temperature is in range
     out_range_counter = 0;
-    tft.setCursor(30, 51);
-    tft.print("IN RANGE");
+    tft_screen.setCursor(30, 51);
+    tft_screen.print("IN RANGE");
     // finaltest = 0;
   }
   else { // this is if the temperature is out of range
     out_range_counter++;
-    tft.setCursor(30, 51);
-    tft.print("OUT OF RANGE");
+    tft_screen.setCursor(30, 51);
+    tft_screen.print("OUT OF RANGE");
     if (out_range_counter >= 5) { // within 5 minutes, with a 15-second interval between measurements
       finaltest = 1; // in startRun(), this is meant to trigger the alarm/notification system -kenton
     }
