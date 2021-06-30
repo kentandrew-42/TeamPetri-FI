db481b556eac17076396d3ce08112ad245b723db
Kenton Roberts <kentandrewr@gmail.com>
2021-06-30 14:05:02 -0500

    Added the thermistor reading file. We can use this for initial temperature reading.

--- PetriFI/TemperatureRead/TemperatureRead.ino
+++ PetriFI/TemperatureRead/TemperatureRead.ino
@@ -14,14 +14,31 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  R1 = 10000
+  float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+  int R1 = 10000; // resistance of reference resistor
+  
   // read the input on analog pin 0:
-  Vo = analogRead(A0); // reads the voltage of ThermistorPin. Resolution of only 4.9 mV (out of 5V)
-  R2 = R1 * (1023.0 / (float)Vo - 1.0);
-  logR2 = log(R2);
-  T = 1.0 / (c1 + (c2 * logR2) + (c3 * pow(logR2, 3)));
+  int Vo = analogRead(A0); // reads the voltage of ThermistorPin. Resolution of only 4.9 mV (out of 5V)
+  Serial.print("----------");
+  Serial.print("Analog Value: ");
+  Serial.println(Vo);
+  
+  float R2 = R1 / ((1023.0 / (float)Vo) - 1.0); // resistance of thermistor
+  Serial.print("Thermistor Resistance:");
+  Serial.println(R2);
+
+  // Minicubator's Model
+  float logR2 = log(R2);
+  float T = 1.0 / (c1 + (c2 * logR2) + (c3 * pow(logR2, 3)));
   T = T - 273.15;
   // print out the value you read:
+  Serial.print("Old Temperature Model:");
   Serial.println(T);
-  delay(1); // delay in between reads for stability
+
+  // Adafruit Thermistor Lookup Table Curve Fit
+  float T_new = log(((R2/1000) / 28.7)) / (-0.0422);
+  Serial.print("New Temperature Model:");
+  Serial.println(T_new);
+
+  delay(1000); // delay in milliseconds between reads
 }
