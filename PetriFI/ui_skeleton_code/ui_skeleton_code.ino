d21cedebb946a9a6fe5c043378a607a5850ef48a
saraJbarker <sjb8@rice.edu>
2021-07-05 13:38:22 -0500

    Added unfinished skeleton code for UI

---
+++ PetriFI/ui_skeleton_code/ui_skeleton_code.ino
@@ -0,0 +1,28 @@
+//Sara Barker, July 5, 2021
+//Team Petri-FI UI Code
+  //This is the very rough draft of the UI functionality code. 
+
+void setup() {
+  // put your setup code here, to run once:
+//will fill this in later once we integrate the UI code into the main code
+}
+
+void loop() {
+  /* This code works by checking the electrical state (i.e. time, temperature) and the button state (i.e. the value
+   *  (0 or 1) of the three buttons).
+   *  
+   *  
+  */
+
+  //check electrical state
+
+  //check button state
+
+  switch
+
+}
+
+void startupScreen() {
+//The first screen - automatically starts up once the battery is connected.
+//Inputs: none, outputs: button press (OK)
+}
